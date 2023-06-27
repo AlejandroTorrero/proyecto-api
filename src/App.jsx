4b4e924f959a08d24816891b1f406ed08c1cb7df
Diego <l19200886@pachuca.tecnm.mx>
2023-06-27 17:18:21 -0600

    Se creo la lista de criptomonedas, se agregó axios y se creó el archivo css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,61 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const API_URL = import.meta.env.VITE_API_URL
+
+  const [cripto, setCripto] = useState()
+
+ /*  useEffect(() => {
+    fetch(`${API_URL}assets`)
+      .then((res) => {
+        return res.json()
+      })
+      .then((data) => {
+        // console.log(data)
+        setCripto(data.data)
+      })
+      .catch(() => {
+        console.error("La petición falló")
+        console.log(API_URL)
+      })
+  }, []) */
+
+  useEffect(() => {
+    axios.get(`${API_URL}assets`)
+      .then((data) => {
+        // console.log(data)
+        // setCripto(data.data)
+        setCripto(data.data.data)
+      })
+      .catch(() => {
+        console.error("La petición falló")
+        console.log(API_URL)
+      })
+  }, []) 
+
+
+
+  if (!cripto) return (
+    <>
+      <span> Cargando... </span>
+    </>
+  )
 
   return (
-    <>      
-      <h1>Hola magali</h1>
+    <>
+      <h1>Lista de criptomonedas</h1>
+
+      <ol>
+        {
+          cripto.map(({ id,name, priceUsd }) => (
+            <li key={id}>Nombre: {name} --- Precio: {priceUsd}</li>
+          ))
+        }
+      </ol>
+
     </>
   )
 }
